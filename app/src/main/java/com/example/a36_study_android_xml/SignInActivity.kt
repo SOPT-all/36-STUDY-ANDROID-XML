149d476457b346e80116fc1e61115186b5029e1e
seungjun <jjw5741@naver.com>
2025-05-26 01:51:12 +0900

    init/ base setting

---
+++ app/src/main/java/com/example/a36_study_android_xml/SignInActivity.kt
@@ -0,0 +1,29 @@
+package com.example.a36_study_android_xml
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.a36_study_android_xml.databinding.ActivitySignInBinding
+
+class SignInActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySignInBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        binding = ActivitySignInBinding.inflate(layoutInflater)
+
+        setContentView(R.layout.activity_sign_in)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+
+    }
+}
