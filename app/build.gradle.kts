21b99d4a43f38886d3ecb0833127932ef5ba1c43
seungjun <jjw5741@naver.com>
2025-05-26 04:22:01 +0900

    feat/ 로그인 로직 구현

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,7 +51,4 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-
-    // material icon
-
 }
