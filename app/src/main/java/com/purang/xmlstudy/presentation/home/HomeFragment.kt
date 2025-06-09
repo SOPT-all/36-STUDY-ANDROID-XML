9cdd363da09db260b44175f57d1dd5647d6a8e28
sonms <smsms5676@naver.com>
2025-06-09 12:43:29 +0900

    feat/viewmodel, recyclerview, adapter 연결

--- app/src/main/java/com/purang/xmlstudy/presentation/home/HomeFragment.kt
+++ app/src/main/java/com/purang/xmlstudy/presentation/home/HomeFragment.kt
@@ -1,81 +1,64 @@
 package com.purang.xmlstudy.presentation.home
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.NavController
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.purang.xmlstudy.R
 import com.purang.xmlstudy.databinding.FragmentHomeBinding
+import com.purang.xmlstudy.presentation.home.adapter.HomeBannerAdapter
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [HomeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class HomeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    private var _binding : FragmentHomeBinding? = null
+    private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
-    private var navController : NavController? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private val navController by lazy { findNavController() }
+    private val adapter by lazy { HomeBannerAdapter() }
+
+    private val viewModel by viewModels<HomeViewModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
-
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        navController = findNavController()
+        super.onViewCreated(view, savedInstanceState)
+        observeData()
+        setupNavigation()
+        setupRecyclerView()
+    }
 
+    private fun observeData() {
+        viewModel.items.observe(viewLifecycleOwner) { items ->
+            adapter.submitList(items)
+        }
+    }
+
+    private fun setupNavigation() {
         binding.profileIv.setOnClickListener {
-            navController?.navigate(R.id.fragment_profile)
+            navController.navigate(R.id.fragment_profile)
+        }
+    }
+
+    private fun setupRecyclerView() {
+        binding.recyclerView.apply {
+            layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+            adapter = this@HomeFragment.adapter
+            setHasFixedSize(true)
         }
     }
 
     override fun onDestroyView() {
-        _binding = null
         super.onDestroyView()
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HomeFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        _binding = null
     }
 }
