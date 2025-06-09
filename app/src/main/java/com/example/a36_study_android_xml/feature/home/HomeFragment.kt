28b3d88afc744afc6855bf3fa09b159fe7c36c0d
seungjun <jjw5741@naver.com>
2025-06-09 14:02:47 +0900

    feat: rcv adapter 바인딩

--- app/src/main/java/com/example/a36_study_android_xml/feature/home/HomeFragment.kt
+++ app/src/main/java/com/example/a36_study_android_xml/feature/home/HomeFragment.kt
@@ -5,8 +5,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.commit
-import com.example.a36_study_android_xml.feature.my.MyFragment
 import com.example.a36_study_android_xml.R
 import com.example.a36_study_android_xml.databinding.FragmentHomeBinding
 import com.example.a36_study_android_xml.feature.main.MainActivity
@@ -27,6 +25,29 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         navigateToMy()
+        bindingRcv()
+    }
+
+    private fun bindingRcv() {
+        val contentsItem = listOf(
+            HomeRcvContentsItem(image = R.drawable.img_home_contents_banner1, index = 1),
+            HomeRcvContentsItem(image = R.drawable.img_home_contents_banner2, index = 2),
+            HomeRcvContentsItem(image = R.drawable.img_home_contents_banner3, index = 3),
+            HomeRcvContentsItem(image = R.drawable.img_home_contents_banner4, index = 4),
+            HomeRcvContentsItem(image = R.drawable.img_home_contents_banner5, index = 5),
+            HomeRcvContentsItem(image = R.drawable.img_home_contents_banner6, index = 6),
+            HomeRcvContentsItem(image = R.drawable.img_home_contents_banner7, index = 7),
+            HomeRcvContentsItem(image = R.drawable.img_home_contents_banner8, index = 8),
+            HomeRcvContentsItem(image = R.drawable.img_home_contents_banner9, index = 9),
+            HomeRcvContentsItem(image = R.drawable.img_home_contents_banner10, index = 10),
+        )
+
+        binding.homeTodayContentsRcv.adapter =
+            HomeContentsItemAdapter(items = contentsItem, isIndexing = true)
+        binding.homeNowContentsRcv.adapter =
+            HomeContentsItemAdapter(items = contentsItem, isIndexing = false)
+        binding.homePopularContentsRcv.adapter =
+            HomeContentsItemAdapter(items = contentsItem, isIndexing = false)
     }
 
     private fun navigateToMy() {
