968eef6f0229a51173e82f1564f7392d95950231
seungjun <jjw5741@naver.com>
2025-06-09 14:02:24 +0900

    feat: rcv adapter 구현

---
+++ app/src/main/java/com/example/a36_study_android_xml/feature/home/HomeRcvContentsItem.kt
@@ -0,0 +1,8 @@
+package com.example.a36_study_android_xml.feature.home
+
+import androidx.annotation.DrawableRes
+
+data class HomeRcvContentsItem(
+    val index: Int? = null,
+    @DrawableRes val image: Int,
+)
